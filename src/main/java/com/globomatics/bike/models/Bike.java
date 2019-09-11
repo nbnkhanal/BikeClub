0cef132ba2b734a35b3ca56e7791d5efd6800ac5
Nabin <mario@Nabins-MacBook-Pro.local>
2019-09-10 22:46:22 -0500

    initial commit

---
+++ src/main/java/com/globomatics/bike/models/Bike.java
@@ -0,0 +1,60 @@
+package com.globomatics.bike.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name= "bike")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Bike {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String name;
+	private String email;
+	private String phone;
+	private String model;
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	
+
+}
